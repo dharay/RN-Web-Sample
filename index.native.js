cbf7d448f0deb7810cfdb7fda8771b1a20e318d7
dharay <dharay.m@gmail.com>
2023-08-23 12:01:09 +0530

    update

---
+++ index.native.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import name from './src/app.json';
+const {appName} = name;
+AppRegistry.registerComponent('webapptest', () => App);
---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView >
+      <Button title='hi'></Button>
+     <Text> hakuna matata </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
